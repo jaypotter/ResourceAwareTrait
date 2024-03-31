d683b5f1ab449a90ff2a8f1891e241d4c26229fb
Jay Potter <j@ypotter.ca>
2024-03-31 18:18:34 -0400

    readResource

--- src/Potter/Resource/Aware/ResourceAwareTrait.php
+++ src/Potter/Resource/Aware/ResourceAwareTrait.php
@@ -8,6 +8,8 @@ use \Psr\Container\ContainerInterface;
 
 trait ResourceAwareTrait 
 {
+    private array $messageBuffer = [];
+    
     final public function getResource(): mixed
     {
         return $this->getContainer()->get('resource');
@@ -16,6 +18,22 @@ trait ResourceAwareTrait
     final public function hasResource(): bool
     {
         return $this->getContainer()->has('resource');
+    }
+    
+    final public function readResource(int $length): string
+    {
+        $messageBuffer = '';
+        
+        while ($message = fread($this->getResource(), $length) && strlen($message) > 0) {
+            $messageBuffer .= $message;
+        }
+        if (strlen($messageBuffer) > 0) {
+            array_push($this->messageBuffer, ...array_values(explode("\r\n", $messageBuffer)));
+        }
+        if (count($this->messageBuffer) == 0) {
+            return '';
+        }
+        return ($this->lastMessage = array_shift($this->messageBuffer)) . PHP_EOL;
     }
     
     abstract public function getContainer(): ContainerInterface;
